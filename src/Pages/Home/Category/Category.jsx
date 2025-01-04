7b6f0e4d7d742f52fb0fa2a45820497b8688ab53
shafriki <shafriki.cse@gmail.com>
2025-01-05 00:19:56 +0600

    added category section

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,57 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import slide1 from '../../../assets/home/slide1.jpg';
+import slide2 from '../../../assets/home/slide2.jpg';
+import slide3 from '../../../assets/home/slide3.jpg';
+import slide4 from '../../../assets/home/slide4.jpg';
+import slide5 from '../../../assets/home/slide5.jpg';
+
+const Category = () => {
+    return (
+        <div className='max-w-screen-lg mx-5 md:mx-auto my-16 '>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                loop={true} 
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination, Autoplay]} 
+                autoplay={{
+                    delay: 1600,
+                    disableOnInteraction: false,
+                }}
+                className="mySwiper"
+            >
+                <SwiperSlide className="relative">
+                    <img src={slide1} alt="slide1" className="w-full md:h-80 object-cover" /> 
+                    <h1 className="text-sm md:text-lg uppercase text-center absolute bottom-0 left-0 right-0 flex items-center justify-center text-semibold text-white bg-opacity-60 bg-black py-2">
+                        Pizza
+                    </h1>
+                </SwiperSlide>
+                <SwiperSlide className="relative">
+                    <img src={slide2} alt="slide2" className="w-full md:h-80 object-cover" /> 
+                    <h1 className="text-sm md:text-lg uppercase text-center absolute bottom-0 left-0 right-0 flex items-center justify-center text-semibold text-white bg-opacity-60 bg-black py-2">
+                        Soup
+                    </h1>
+                </SwiperSlide>
+                <SwiperSlide className="relative">
+                    <img src={slide3} alt="slide3" className="w-full md:h-80 object-cover" /> 
+                    <h1 className="text-sm md:text-lg uppercase text-center absolute bottom-0 left-0 right-0 flex items-center justify-center text-semibold text-white bg-opacity-60 bg-black py-2">
+                        Vegetable
+                    </h1>
+                </SwiperSlide>
+                <SwiperSlide className="relative">
+                    <img src={slide4} alt="slide4" className="w-full md:h-80 object-cover" /> 
+                    <h1 className="text-sm md:text-lg uppercase text-center absolute bottom-0 left-0 right-0 flex items-center justify-center text-semibold text-white bg-opacity-60 bg-black py-2">
+                        Cake
+                    </h1>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer className="footer bg-base-200 text-base-content p-10">
+            <footer className="footer bg-black text-white p-10">
             <nav>
                 <h6 className="footer-title">Services</h6>
                 <a className="link link-hover">Branding</a>
@@ -25,7 +25,7 @@ const Footer = () => {
                 <a className="link link-hover">Cookie policy</a>
             </nav>
             </footer>
-            <footer className="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
+            <footer className="footer bg-black text-white border-base-300 border-t px-10 py-4">
             <aside className="grid-flow-col items-center">
                 <svg
                 width="24"
