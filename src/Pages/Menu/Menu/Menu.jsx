6aea3782a5d3e4363ba3f559ab7749ea357c0f13
shafriki <shafriki.cse@gmail.com>
2025-01-05 04:18:59 +0600

    added cover in menu page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Our Menu | Bistro Boss</title>
             </Helmet>
-            our menu
+            <Cover image={menuImg} title="our menu"></Cover>
         </div>
     );
 };
