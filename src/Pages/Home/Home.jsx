53a53b6796f2d116c630d9ed31cd8b43163af8a2
shafriki <shafriki.cse@gmail.com>
2025-01-05 01:32:41 +0600

    popular item section added and menu json data added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
 import BistroSection from './BistroSection/BistroSection';
+import PopularItem from './Menu/PopularItem';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Category></Category>
             <BistroSection></BistroSection>
+            <PopularItem></PopularItem>
         </div>
     );
 };
---
+++ src/Pages/Home/Menu/PopularItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle';
+
+const PopularItem = () => {
+    return (
+        <div>
+            <SectionTitle heading={'Popular Menu'} subHeading={'Check It Out'}>
+            </SectionTitle>
+        </div>
+    );
+};
+
+export default PopularItem;
