539a8022774df8347603a4525e5ee2f8ea817c2e
shafriki <shafriki.cse@gmail.com>
2025-01-04 15:59:28 +0600

    router setup complete of bistro boss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { router } from './Routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
